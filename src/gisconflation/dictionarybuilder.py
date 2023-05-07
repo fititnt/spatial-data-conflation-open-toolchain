6f66badde18489cec56b44679409d9d280f1b486
Emerson Rocha <rocha@ieee.org>
2023-05-06 21:36:07 -0300

    json2csv: created

--- src/gisconflation/dictionarybuilder.py
+++ src/gisconflation/dictionarybuilder.py
@@ -15,7 +15,7 @@
 #         NOTES:  ---
 #       AUTHORS:  Emerson Rocha <rocha[at]ieee.org>
 # COLLABORATORS:  ---
-# W
+#
 #       COMPANY:  EticaAI
 #       LICENSE:  GNU Affero General Public License v3.0 or later
 #                 SPDX-License-Identifier: AGPL-3.0-or-later
